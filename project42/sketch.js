5d8ca9df233754eae6758addb3884c3404ab59a6
samriddhi7307 <72978417+samriddhi7307@users.noreply.github.com>
2021-01-07 12:44:54 +0530

    Add files via upload

--- project42/sketch.js
+++ project42/sketch.js
@@ -7,12 +7,11 @@
   var thunder;
 
   function preload(){
- thunder1 = loadImage("thunder.images/1.png");
- thunder2 = loadImage("thunder.images/2.png");
- thunder3 = loadImage("thunder.images/3.png");
- thunder4 = loadImage("thunder.images/4.png");
+ thunder1 = loadImage("../thunder.images/1.png");
+ thunder2 = loadImage("../thunder.images/2.png");
+ thunder3 = loadImage("../thunder.images/3.png");
+ thunder4 = loadImage("../thunder.images/4.png");
   }
-
   function setup() {
     createCanvas(500, 750);
     engine = Engine.create();
@@ -27,11 +26,12 @@
     Engine.update(engine);
     rand = Math.round(random(1,4));
     thunderCreatedFrame=frameCount;
-    if(thunderCreatedFrame + 10 ===frameCount && thunder){ 
-      thunder.destroy(); 
-    }
+    
     if(frameCount%80===0){
         thunder = createSprite(random(10,370), random(10,30), 10, 10);
+        if(thunderCreatedFrame + 10 ===frameCount && thunder){ 
+          thunder.destroy(); 
+        }
         switch(rand){
             case 1: thunder.addImage(thunder1);
             break;
@@ -45,24 +45,6 @@
         }
         thunder.scale = random(0.3,0.6)
     }
-
-   /* if(frameCount%80===0){ 
-      thunderCreatedFrame=frameCount;
-       thunder = createSprite(random(10,370), random(10,30), 10, 10); 
-       switch(rand){
-          case 1: thunder.addImage(thunder1);
-           break;
-          case 2: thunder.addImage(thunder2);
-          break;
-          case 3: thunder.addImage(thunder3);
-          break;
-          case 4: thunder.addImage(thunder4);
-           break;
-           default: break;
-       }
-      }
-      */
-
 
       raindrop.push(new raindrops(random(100, 10),0,5));
       //score++;
